9f7e620e9c29694978b721154f4610a4e1555a9f
Vadym Skoryk <yopt322@gmail.com>
2018-10-09 04:24:18 +0300

    Commit from IDE

--- src/TestRepo.java
+++ src/TestRepo.java
@@ -5,5 +5,6 @@ public class TestRepo {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello from Git");
+        System.out.println("Hello from IDE");
     }
 }
